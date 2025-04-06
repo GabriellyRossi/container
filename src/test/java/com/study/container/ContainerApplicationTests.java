252d68a18131de4354bdb5436307dc0bf72fb21c
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-04-06 14:04:38 -0300

    first commit

---
+++ src/test/java/com/study/container/ContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
